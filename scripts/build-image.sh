51fb1ba5d37e6509d09d945b98b3ec635a54e1e2
locnguyenvu-mck <locnguyenvu.mck@gmail.com>
2023-06-09 10:52:31 +0700

    feat 01 : Update container image build

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -4,3 +4,8 @@ echo 'PROJECT_ID: -> ' ${PROJECT_ID:-'not set'}
 echo 'TRIGGER_NAME: -> ' ${TRIGGER_NAME:-'not set'}
 echo 'BRANCH_NAME: -> ' ${BRANCH_NAME:-'not set'}
 
+IMAGE_NAME="gcr.io/${PROJECT_ID}/${TRIGGER_NAME}/${BRANCH_NAME//\//-}"
+
+docker build -t "${IMAGE_NAME}:${SHORT_SHA}" .
+
+docker push "${IMAGE_NAME}:${SHORT_SHA}"
